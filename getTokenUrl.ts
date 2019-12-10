71d69755a8be0e47442f430e0209e2417e449682
Alexey Nagorny <anagorny@wavesplatform.com>
2019-12-10 20:24:40 +0300

    first commit

--- getTokenUrl.ts
+++ getTokenUrl.ts
@@ -1,5 +1,3 @@
 require('dotenv').config();
-const scope = ['wall','messages','offline'];
-
 const url = `https://oauth.vk.com/authorize?client_id=${process.env.CLIENT}&scope=notify,photos,friends,audio,video,notes,pages,docs,status,questions,offers,wall,groups,messages,notifications,stats,ads,offline&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token`
 console.log(url)
